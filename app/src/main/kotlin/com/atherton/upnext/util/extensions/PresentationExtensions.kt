d79821f7eee1b5c86a0f1e27d28a87e584e7d54a
darrenatherton <darren.atherton49@yahoo.com>
2019-03-26 19:22:28 +0000

    Add long press and double tap extension function

--- app/src/main/kotlin/com/atherton/upnext/util/extensions/PresentationExtensions.kt
+++ app/src/main/kotlin/com/atherton/upnext/util/extensions/PresentationExtensions.kt
@@ -1,8 +1,29 @@
 package com.atherton.upnext.util.extensions
 
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
 fun <T> Observable<T>.preventMultipleClicks(): Observable<T> {
     return this.throttleFirst(300, TimeUnit.MILLISECONDS)
+}
+
+inline fun View.setOnGesturesListener(crossinline onDoubleTap: () -> Unit, crossinline onLongPress: () -> Unit) {
+    val listener = object : GestureDetector.SimpleOnGestureListener() {
+        override fun onDoubleTap(e: MotionEvent): Boolean {
+            onDoubleTap.invoke()
+            return true
+        }
+
+        override fun onLongPress(e: MotionEvent) {
+            onLongPress.invoke()
+        }
+    }
+    val detector = GestureDetector(context, listener).apply {
+        setOnDoubleTapListener(listener)
+        setIsLongpressEnabled(true)
+    }
+    this.setOnTouchListener { _, event -> detector.onTouchEvent(event) }
 }
